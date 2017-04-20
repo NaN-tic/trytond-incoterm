7bb42894d589cc7abcd297a053df0cfe8248589f
Raimon Esteve <raimon@nan-tic.com>
2017-04-20 23:37:01 +0200

    FIX test class

--- tests/test_incoterm.py
+++ tests/test_incoterm.py
@@ -5,12 +5,13 @@ import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
 
 
-class TestCase(ModuleTestCase):
-    'Test module'
+class IncotermTestCase(ModuleTestCase):
+    'Test Incoterm module'
     module = 'incoterm'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
+        IncotermTestCase))
     return suite
