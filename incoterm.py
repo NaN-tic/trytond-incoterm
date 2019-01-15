37d1fe30bf83bb92db8660603e59549b68d445e4
Maxime Richez <mrichez@saluc.com>
2019-01-15 11:30:36 +0100

    Adding place_required field to force incoterm_place for some incoterms

--- incoterm.py
+++ incoterm.py
@@ -1,5 +1,5 @@
-#This file is part of Tryton.  The COPYRIGHT file at the top level of
-#this repository contains the full copyright notices and license terms.
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
 from trytond.model import ModelView, ModelSQL, fields
 
 __all__ = ['Category', 'Incoterm']
@@ -19,10 +19,16 @@ class Incoterm(ModelSQL, ModelView):
     code = fields.Char('Code', required=True)
     active = fields.Boolean('Active', select=True)
     category = fields.Many2One('incoterm.category', 'Category')
+    place_required = fields.Boolean('Place required',
+        help="Make place required for this incoterm")
 
     @staticmethod
     def default_active():
         return True
+
+    @staticmethod
+    def default_place_required():
+        return False
 
     def get_rec_name(self, name):
         return '%s - %s' % (self.code, self.name)
--- party.py
+++ party.py
@@ -1,14 +1,21 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
+
 from trytond import backend
 from trytond.pool import PoolMeta, Pool
 from trytond.model import fields, ModelSQL, ValueMixin
+from trytond.pyson import Eval, Bool
 from trytond.tools.multivalue import migrate_property
 
 __all__ = ['Party', 'PartyIncoterm']
 
 incoterm = fields.Many2One('incoterm', 'Incoterm')
-incoterm_place = fields.Char('Incoterm Name Place')
+incoterm_place = fields.Char('Incoterm Name Place',
+        states={
+            'required': Bool(Eval('place_required')),
+            'invisible': ~Bool(Eval('place_required')),
+            },
+        depends=['place_required'])
 
 
 class Party(metaclass=PoolMeta):
@@ -17,6 +24,8 @@ class Party(metaclass=PoolMeta):
     incoterm_place = fields.MultiValue(incoterm_place)
     incoterms = fields.One2Many(
         'party.party.incoterm', 'party', "Incoterms")
+    place_required = fields.Function(fields.Boolean('Place Required'),
+        'on_change_with_place_required')
 
     @classmethod
     def multivalue_model(cls, field):
@@ -24,6 +33,15 @@ class Party(metaclass=PoolMeta):
         if field in {'incoterm', 'incoterm_place'}:
             return pool.get('party.party.incoterm')
         return super(Party, cls).multivalue_model(field)
+
+    @fields.depends('incoterm')
+    def on_change_with_place_required(self, name=None):
+        if self.incoterm:
+            return self.incoterm.place_required
+
+    @fields.depends('incoterm', 'incoterm_place')
+    def on_change_incoterm(self, name=None):
+        self.incoterm_place = None
 
 
 class PartyIncoterm(ModelSQL, ValueMixin):
